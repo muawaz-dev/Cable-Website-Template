7c6c2218c2528ef95561502f4178b91ffe4b872c
Muawaz Ahmad <muawaz8@gmail.com>
2026-03-12 14:48:59 +0500

    Changed the top nav

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -50,7 +50,7 @@ export default function Navbar() {
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
               </svg>
-              jeff@hardencommunications.com
+              abc@gmail.com
             </a>
             {/* Phone */}
             <a
@@ -61,7 +61,7 @@ export default function Navbar() {
               <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
-              (303) 668-5150
+              (999) 999-9999
             </a>
           </div>
           {/* Social Icons */}
